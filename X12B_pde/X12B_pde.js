b56fedde76431e479d3ffc905432ac6d8a6fc35c
hugo <hugo@francis.home>
2017-11-14 16:39:14 +0100

    Ajout LCS

--- X12B_pde/X12B_pde.js
+++ X12B_pde/X12B_pde.js
@@ -13,3 +13,17 @@ function ed(s1,s2)
 
   return "Reponse a retourner";
 }
+
+function lcs(ch1, ch2) {
+  var n = ch1.length-1;
+  var m = ch2.length-1;
+  if (ch1==="" || ch2==="") {
+    return 0;
+  }   
+
+  if (ch1.charAt(n) === ch2.charAt(m)) {
+    return (1+lcs(ch1.substr(0, n), ch2.substr(0, m)));
+  } else {
+    return max(lcs(ch1.substr(0, n+1), ch2.substr(0, m)), lcs(ch1.substr(0, n), ch2.substr(0, m+1)));
+  }
+}
