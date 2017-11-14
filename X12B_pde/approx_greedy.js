365874de9fc7783cc36ebae98ae6314d5abbc8df
Clement <clement.colin69@gmail.com>
2017-11-14 18:59:42 +0100

    Greedy fonctionne

--- X12B_pde/approx_greedy.js
+++ X12B_pde/approx_greedy.js
@@ -1,5 +1,13 @@
 function approx_greedy(str1, str2)
 {
-  
-  return "Not working";
+  var sum = 0;
+  for(var i = 0; i < Math.min(str1.length,str2.length);i++)
+  {
+    if(str1.charAt(i)!=str2.charAt(i))
+    {
+      sum ++;
+    }
+  }
+  sum += Math.abs(str1.length-str2.length);
+  return sum;
 }
