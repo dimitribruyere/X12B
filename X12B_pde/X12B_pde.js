788e3fff80f5d0f1d1ff33ac4e14e7204837b5be
Chris <chrisjahim@free.fr>
2017-11-14 17:31:18 +0100

    add: Ajout choix algo

--- X12B_pde/X12B_pde.js
+++ X12B_pde/X12B_pde.js
@@ -8,20 +8,29 @@ function draw()
   
 }
 
-function ed(s1,s2)
+function rec(ch1, ch2)
 {
-
-  return "Reponse a retourner";
+  
+  return "Non fini";
 }
 
-function lcs(ch1, ch2) {
+function classic(ch1, ch2)
+{
+  
+  return "Non fini";
+}
+
+function lcs(ch1, ch2)
+{
   var n = ch1.length-1;
   var m = ch2.length-1;
-  if (ch1==="" || ch2==="") {
+  if (ch1==="" || ch2==="")
+  {
     return 0;
   }   
 
-  if (ch1.charAt(n) === ch2.charAt(m)) {
+  if (ch1.charAt(n) === ch2.charAt(m))
+  {
     return (1+lcs(ch1.substr(0, n), ch2.substr(0, m)));
   } else {
     return max(lcs(ch1.substr(0, n+1), ch2.substr(0, m)), lcs(ch1.substr(0, n), ch2.substr(0, m+1)));
