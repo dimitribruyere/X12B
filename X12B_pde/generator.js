468fd98c44663ada9b03a53cdc309ff9fed1c922
hugo <hugo@francis.home>
2017-12-08 17:13:53 +0100

    worked on string generation

--- X12B_pde/generator.js
+++ X12B_pde/generator.js
@@ -1,26 +1,50 @@
 function generator() {
-  this.create2Strings = function(String1Length, String2Length) {
-    var string1  = "";
-    var string2  = "";
+
+  this.stringSet1         = [];
+  this.stringSet2         = [];
+  this.editDistSet        = [];
+  this.computationTimeSet = [];
+
+  this.generateStrings = function(howMany) {
+    for (var i = 0; i < howMany; i++ ) {
+      var newString = this.createString(10); // TO CHANGE Hard-coded value TO CHANGE //
+      this.stringSet1.push(newString);
+      newString = this.createString(10); // TO CHANGE Hard-coded value TO CHANGE //
+      this.stringSet2.push(newString);
+    }
+  }
+
+  this.showStrings = function() {
+    for (var i = 0; i < this.stringSet1.length; i++) {
+      timerStart = new Date();
+      computeAlgo = classic(this.stringSet1[i], this.stringSet2[i]);
+      timerEnd = new Date();
+      var editDist = computeAlgo.cost;
+      computationTime = timerEnd - timerStart;
+      this.computationTimeSet[i] = computationTime;
+      this.editDistSet[i] = editDist;
+      //alert(this.stringSet1[i] +" || "+ this.stringSet2[i] + " || editDist : "+editDist+" || time : "+computationTime);
+      
+    }
+  }
+
+  this.createString = function(StringLength) {
+    var string   = "";
     var alphabet = [];
+
+    // Alphabet generation :
     for (var i = 0; i < 26; i++) {
       alphabet.push(String.fromCharCode(i+"a".charCodeAt(0)));
     }
     for (var i = 0; i < 26; i++) {
       alphabet.push(String.fromCharCode(i+"A".charCodeAt(0)));
     }
-    for (var i = 0; i < String1Length; i++) {
-      randomChar = random(alphabet);
-      string1 += randomChar;
+
+    // String generation :
+    for (var i = 0; i < StringLength; i++) {
+      var randomChar = random(alphabet);
+      string += randomChar;
     }
-    for (var i = 0; i < String2Length; i++) {
-      randomChar = random(alphabet);
-      string2 += randomChar;
-    }
-  var strings = {"string1": 
-  string1, "string2": 
-    string2
-  };
-  return strings;
-}
+    return string;
+  }
 }
