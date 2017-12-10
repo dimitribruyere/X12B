8e3524a15ad8c50416cd79dba8bce49ccb1d69f7
dimitribruyere <dimitri.bruyere@outlook.fr>
2017-12-10 12:46:12 +0100

    backtracking du greedy

--- X12B_pde/approx_greedy.js
+++ X12B_pde/approx_greedy.js
@@ -1,14 +1,30 @@
 function approx_greedy(str1, str2)
 {
   //On regarde chaque caractère un à un, si ils sont les mêmes on ne fait rien, sinon on remplace celui du plus court. Enfin, on additionne la différence de longueur des deux
-  var sum = 0;
+  var ed = 0;
+  var path = "";
   for(var i = 0; i < Math.min(str1.length,str2.length);i++)
   {
+    path+='S';
     if(str1.charAt(i)!=str2.charAt(i))
     {
-      sum ++;
+      ed ++;
     }
   }
-  sum += Math.abs(str1.length-str2.length);
-  return sum;
+  ed += Math.abs(str1.length-str2.length);
+  if (str1.length > str2.length)
+  {
+    for (i=str2.length; i<str1.length; i++)
+    {
+      path+='-';
+    }
+  }
+  else
+  {
+    for (i=str1.length; i<str2.length; i++)
+    {
+      path+='+';
+    }
+  }
+  return { "ed" : ed, "path" : path};
 }
