a5dd084aa5d34d61e43b3d3e4f19870e23e1cc65
hugo <hugo@francis.home>
2017-12-12 03:15:25 +0100

    revamped completely divide and conquer, almost working, still buggy in some cases...

--- X12B_pde/divide_conquer.js
+++ X12B_pde/divide_conquer.js
@@ -1,23 +1,34 @@
-var divideAndConquerPathCoordinateArray = [];
-var divideAndConquerEditDistance = -1;
+var divideAndConquerEditDistance;
 function divide_conquer(str1, str2) {
-  divide_conquer_bis(str1, str2);
-  alert(divideAndConquerPathCoordinateArray);
+  divideAndConquerEditDistance=-1;
+  p =divide_conquer_bis(str1, str2);
+  //alert("wtf p ?? : "+p);
   return {
   "ed":
   divideAndConquerEditDistance, "path":
-    divideAndConquerPathCoordinateArray
+    p
   };
 }
 
 function divide_conquer_bis(str1, str2)
 {
-  alert("str1 : "+str1+" str2 : "+str2);
+  //alert("Appel sur :\nstr1 : "+str1+" str2 : "+str2);
 
+  // If length of str2 is 2 or less, we stop the recursion and find the edit distance between a VERY small str2 ( <= 2 ) and a QUITE small str1 (because it had been recursively cut down
+
+  if (str2.length <= 2) {
+    var classicCallOnSmallProblem = classic(str1, str2);
+    var path_bit = classicCallOnSmallProblem.path;
+    if (divideAndConquerEditDistance === -1) {
+      divideAndConquerEditDistance = classicCallOnSmallProblem.ed;
+    }
+    return  path_bit;
+  }
 
   var top_row = [];
   var bot_row = [];
   var tmp_row = [];
+  var whereToCutStr2 = str2.length / 2;
 
   // We initialize top row and bot row:
   for ( var i = 0; i <= str1.length; i++ ) {
@@ -25,20 +36,21 @@ function divide_conquer_bis(str1, str2)
     bot_row[i] = i;
   }
 
-  alert(str2.charAt((str2.length / 2)-1));
-  // We find the row at position length2 /2 starting from the top  
-  for ( var i = 0; i < str2.length / 2; i++ ) {
+  //alert(str2.charAt((str2.length / 2)-1));
+  // We find the row at position length2 /2 starting from the top 
+
+  for ( var i = 0; i <= whereToCutStr2; i++ ) {
     find_next(top_row, tmp_row, str1, str2.charAt(i));
     var permutateTmpAndTop = top_row;
     top_row = tmp_row;
     tmp_row = permutateTmpAndTop;
   }
 
-
   // We find the row at position length2 /2 starting from the bottom on the reversed strings :
   var str1Reverse = reverseString(str1);
   var str2Reverse = reverseString(str2);
-  for ( var i = 0; i < (1+str2.length) / 2; i++ ) {
+
+  for ( var i = 0; i < whereToCutStr2; i++ ) {
     find_next(bot_row, tmp_row, str1Reverse, str2Reverse.charAt(i));
     var permutateTmpAndBot = bot_row;
     bot_row = tmp_row;
@@ -46,51 +58,85 @@ function divide_conquer_bis(str1, str2)
   }
 
   // We merge both rows into one and look for the min : tmp = bot + top
-  //alert(bot_row);
   //alert(top_row);
+  //alert(bot_row);
+
   for (var i = 0; i < top_row.length; i++ ) {
     tmp_row[i] = bot_row[top_row.length-i] + top_row[i];
   }
-  tmp_row[0] = 2*top_row[0];
+  tmp_row[0] = str2.length+str1.length; // first cell of top row is the length of str2 i.e. worse case edit distance
 
-  var whereToCut = 0 ;
+  var whereToCutStr1 = 0 ;
   var currentMin = Number.MAX_SAFE_INTEGER;
+
+  //alert(tmp_row);
 
   for (var i = 0; i < tmp_row.length; i++) { // To know where to cut s1 we look for argmin (tmp_row) 
     if ( tmp_row[i] <= currentMin ) {
-      whereToCut = i; 
+      whereToCutStr1 = i; 
       currentMin = tmp_row[i];
     }
   }
 
-  //return whereToCut
-
   if (divideAndConquerEditDistance === -1) {
-    divideAndConquerEditDistance = tmp_row[whereToCut];
+    divideAndConquerEditDistance = tmp_row[whereToCutStr1];
+    //alert("ED :"+divideAndConquerEditDistance);
   }
-  whereToCut--;
-  divideAndConquerPathCoordinateArray[str2.length / 2]=whereToCut;
-  alert("Where to cut ?"+whereToCut);
 
-  var Part1OfStr1 = str1.substr(0, 1+str1.length/2);
-  var Part1OfStr2 = str2.substr(0, whereToCut     );
-  var Part2OfStr1 = str1.substr(1+str1.length/2, str1.length    );
-  var Part2OfStr2 = str2.substr(whereToCut, str2.length    );
+  whereToCutStr1--;
+  //alert("whereToCut str1 : "+whereToCutStr1+" that's letter"+str1.charAt(whereToCutStr1));
+  whereToCutStr1 = max(whereToCutStr1, 0);
+  //if (whereToCut===0) {
+  //  alert("Stop !");
+  //  return;
+  //}
+
+  //divideAndConquerPathCoordinateArray[str2.length / 2]=whereToCut;
+  //alert("Where to cut ?"+whereToCut);
+
+  var Part1OfStr1 = str1.substr(0, whereToCutStr1);
+  var Part1OfStr2 = str2.substr(0, whereToCutStr2);
+  var Part2OfStr1 = str1.substr(whereToCutStr1, str1.length);
+  var Part2OfStr2 = str2.substr(whereToCutStr2, str2.length);
   //alert("p1s1 : "+Part1OfStr1+" p1s2 "+Part1OfStr2+" p2s1 "+Part2OfStr1+"p2s2"+Part2OfStr2);
 
-  if (Part1OfStr1.length != 0 && Part1OfStr2.length != 0) {
-    if (Part1OfStr2.length === 1) {
-//Compléter le tableau divideAndConquerPathCoordinateArray ?! ?
-    } else {
-      divide_conquer_bis(Part1OfStr1, Part1OfStr2);
-    }
+  //alert("(1):Appels récursifs sur\n"+str1+"==>"+Part1OfStr1+"\net\n"+str2+"==>"+Part1OfStr2+"\net sur\n"+str1+"==>"+Part2OfStr1+"\net\n"+str2+"==>"+Part2OfStr2);
+
+
+  var path_tmp="";
+  if (Part1OfStr1.length != 0 ) {//&& Part1OfStr2.length != 0) {
+    var rajout = divide_conquer_bis(Part1OfStr1, Part1OfStr2);
+    //alert("(1) "+str1+" et "+str2+" me donnent : "+rajout);
+    path_tmp += rajout;
+  } else { // We have to add the first part of str2 to the second part so that it is treated !
+
+    Part2OfStr2 = Part1OfStr2+Part2OfStr2; 
+    alert(Part2OfStr2);
+    //rajout = "";
+    //for (var i = 0 ; i < Part1OfStr2.length ; i++) {
+    // rajout += "+"; 
+    //}
+    ////alert("(1) ragout ? "+rajout);
+    // path_tmp += rajout;
   }
-  if (Part2OfStr1.length != 0 && Part2OfStr2.length != 0) {
-    if (Part2OfStr2.length === 1) {
-    } else {
-      divide_conquer_bis(Part2OfStr1, Part2OfStr2);
+  //alert("(2):Appels récursifs sur\n"+str1+"==>"+Part1OfStr1+"\net\n"+str2+"==>"+Part1OfStr2+"\net sur\n"+str1+"==>"+Part2OfStr1+"\net\n"+str2+"==>"+Part2OfStr2);
+
+  if (Part2OfStr1.length != 0  ) {//&& Part2OfStr2.length != 0) {
+
+
+    var rajout = divide_conquer_bis(Part2OfStr1, Part2OfStr2);
+    //alert("(2) "+str1+" et "+str2+" me donnent : "+rajout);
+    path_tmp += rajout;
+  } else {
+    rajout = "";
+    for (var i = 0; i < Part2OfStr2.length; i++) {
+      rajout += "+";
     }
+    //alert("(2) ragout ? "+rajout);
+
+    path_tmp += rajout;
   }
+  return path_tmp;
 }
 
 function find_next(top_row, tmp_row, str1, charOfStr2) {
@@ -113,7 +159,7 @@ function reverseString(str) {
 }
 
 /*
-    A B C D E <- str1
+     A B C D E <- str1
  0 1 2 3 4 5 <- top row
  G 1
  H 2
